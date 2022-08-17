eb25cf3fa680715db8dd9e522fb4e97b13ca06ed
Talley Lambert <talley.lambert@gmail.com>
2022-08-17 15:15:19 -0400

    add github data and update main reindex

--- lib/pyapi/core.py
+++ lib/pyapi/core.py
@@ -4,12 +4,10 @@ Currently, it's not used anywhere, just for local interactive convenience.
 """
 
 from __future__ import annotations
-import os
 import json
-import re
-from itertools import chain
 from pathlib import Path
-from typing import TYPE_CHECKING, Literal, Optional, Tuple
+from typing import TYPE_CHECKING
+
 
 if TYPE_CHECKING:
     from typing import TypedDict
@@ -26,7 +24,6 @@ if TYPE_CHECKING:
 
 
 PUBLIC = Path(__file__).parent.parent.parent / "public"
-GITHUB_RE = re.compile(r"https://github\.com/([^/]+)/([^/#@]+)")
 
 
 def plugins() -> Plugins:
@@ -37,62 +34,6 @@ def plugins() -> Plugins:
 def active_plugins() -> list[PluginName]:
     """Return a list of active plugins."""
     return plugins()["active"]
-
-
-def github_org_repo(name: PluginName) -> Optional[Tuple[str, str]]:
-    """Return the link to the public repo."""
-    try:
-        info: dict = pypi_info(name)["info"]
-    except KeyError:
-        return None
-
-    links = chain(
-        [info.get("home_page"), info.get("package_url"), info.get("project_url")],
-        (info.get("project_urls") or {}).values(),
-    )
-    for link in links:
-        if match := GITHUB_RE.match(link):
-            org, repo = match.groups()
-            if repo.endswith(".git"):
-                repo = repo[:-4]
-            return org, repo
-
-
-GITHUB_ENDPOINTS = Literal[
-    "assignees",
-    "branches",
-    "commits",
-    "commits/HEAD",
-    "contents",
-    "contributors",
-    "forks",
-    "events",
-    "issues",
-    "languages",
-    "license",
-    "pulls",
-    "readme",
-    "releases",
-    "stargazers",
-    "subscribers",
-    "tags",
-    "teams",
-]
-
-
-def github_info(name: PluginName, endpoint: str = "") -> dict:
-    """Fetch information from github api."""
-    import requests
-
-    if not (github_info := github_org_repo(name)):
-        raise ValueError(f"No github repo for {name}")
-
-    endpoint = f"/{endpoint}" if endpoint else ""
-    url = "https://api.github.com/repos/{}/{}{}".format(*github_info, endpoint)
-    headers = {"Accept": "application/vnd.github+json"}
-    if GITHUB_API_TOKEN := os.environ.get("GITHUB_API_TOKEN"):
-        headers["Authorization"] = f"token {GITHUB_API_TOKEN}"
-    return requests.get(url, headers=headers).json()
 
 
 def manifest(name: PluginName) -> PluginManifest:
